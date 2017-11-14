59c22459a7413ce3d9bb3d66c161a2606a008d79
srini-x <srini-x@users.noreply.github.com>
2017-11-14 11:49:28 -0800

    PID class has been implemented

--- src/PID.cpp
+++ src/PID.cpp
@@ -10,12 +10,20 @@ PID::PID() {}
 
 PID::~PID() {}
 
-void PID::Init(double Kp, double Ki, double Kd) {
+void PID::Init(double Kp_in, double Ki_in, double Kd_in) {
+  Kp = Kp_in;
+  Kd = Kd_in;
+  Ki = Ki_in;
 }
 
 void PID::UpdateError(double cte) {
+  d_error = cte - p_error;
+  p_error = cte;
+  i_error += cte;
 }
 
 double PID::TotalError() {
+  double total_error = -Kp * p_error - Kd * d_error - Ki * i_error;
+  return total_error;
 }
 
--- src/PID.h
+++ src/PID.h
@@ -12,7 +12,7 @@ public:
 
   /*
   * Coefficients
-  */ 
+  */
   double Kp;
   double Ki;
   double Kd;
@@ -30,7 +30,7 @@ public:
   /*
   * Initialize PID.
   */
-  void Init(double Kp, double Ki, double Kd);
+  void Init(double Kp_in, double Ki_in, double Kd_in);
 
   /*
   * Update the PID error variables given cross track error.
